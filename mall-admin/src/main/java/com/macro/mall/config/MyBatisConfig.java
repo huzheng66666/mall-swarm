c189dacafc825daa7c98f76dbe241b0de8af39a9
huzheng <987111808@qq.com>
2024-08-03 00:30:26 +0800

    fix: 修改主干代码

--- mall-admin/src/main/java/com/macro/mall/config/MyBatisConfig.java
+++ mall-admin/src/main/java/com/macro/mall/config/MyBatisConfig.java
@@ -12,8 +12,5 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan({"com.macro.mall.mapper","com.macro.mall.dao"})
 public class MyBatisConfig {
-    private Integer age;
-    private Integer sex;
-    private String a;
-    private String c;
+    private String age;
 }
